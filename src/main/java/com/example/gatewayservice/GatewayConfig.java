ca1a895b0a2f1dc1106c4bae8f5d26fc4554e6d7
nhthong2001 <nhthong30@gmail.com>
2022-06-29 10:07:02 +0700

    Initial commit

---
+++ src/main/java/com/example/gatewayservice/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.example.gatewayservice;
+import com.example.gatewayservice.filter.AuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class GatewayConfig {
+
+    @Autowired
+    private AuthFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes().route("auth", r -> r.path("/auth/**").filters(f -> f.filter(filter)).uri("http://localhost:9091"))
+                .route("service-a", r -> r.path("/api/**").filters(f -> f.filter(filter)).uri("http://localhost:9099")).build();
+    }
+
+}
---
+++ src/main/java/com/example/gatewayservice/filter/AuthFilter.java
@@ -0,0 +1,84 @@
+package com.example.gatewayservice.filter;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class AuthFilter implements GatewayFilter {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public Claims getClaims(final String token) {
+        try {
+            Claims body = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+            return body;
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " => " + e);
+        }
+        return null;
+    }
+
+    public void validateToken(final String token) throws Exception {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+        } catch (SignatureException ex) {
+            throw new Exception("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            throw new Exception("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            throw new Exception("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            throw new Exception("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            throw new Exception("JWT claims string is empty.");
+        }
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
+
+        final List<String> apiEndpoints = Arrays.asList("/register", "/login");
+
+        Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
+                .noneMatch(uri -> r.getURI().getPath().contains(uri));
+
+        if (isApiSecured.test(request)) {
+            if (!request.getHeaders().containsKey("Authorization")) {
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+                return response.setComplete();
+            }
+
+            final String token = request.getHeaders().getOrEmpty("Authorization").get(0);
+
+            try {
+                validateToken(token);
+            } catch (Exception e) {
+
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+                return response.setComplete();
+            }
+
+            Claims claims = getClaims(token);
+            exchange.getRequest().mutate().header("id", String.valueOf(claims.get("id"))).build();
+        }
+
+        return chain.filter(exchange);
+    }
+}
